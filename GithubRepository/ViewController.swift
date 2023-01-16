860158740254da20b970eae99eeb3700d69863e2
seongzzang <87599027+seongzzang@users.noreply.github.com>
2023-01-16 23:19:00 +0900

    Initial Commit

---
+++ GithubRepository/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GithubRepository
+//
+//  Created by 양성혜 on 2023/01/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
